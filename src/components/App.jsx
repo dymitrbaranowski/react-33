90e8327cfa3ed6c7758a824e5a9c7bf926e4abe1
Dymitr Baranowski <125156435+dymitrbaranowski@users.noreply.github.com>
2024-01-02 19:17:24 +0200

    fox3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,12 @@
 import { PageTitle } from 'components/PageTitle/PageTitle';
 import { EventBoard } from 'components/EventBoard/EventBoard';
-import upcomingEvents from '../index';
+import upcomingEvents from './upcoming-events.json';
 
 export const App = () => {
   return (
-    <div>
+    <>
       <PageTitle text="24th Core Worlds Coalition Conference" />
-      <EventBoard events={[1, 2, 3, 4, 5]} />
-    </div>
+      <EventBoard events={upcomingEvents} />
+    </>
   );
 };
--- src/components/EventBoard/EventBoard.jsx
+++ src/components/EventBoard/EventBoard.jsx
@@ -1,6 +1,20 @@
-import css from './EventBoard.module.css';
+import { Event } from 'components/Event/Event';
+import css from 'components/EventBoard/EventBoard.module.css';
 
 export const EventBoard = ({ events }) => {
-  console.log(events);
-  return <div className={css.eventBoard}>Event cards</div>;
+  return (
+    <div className={css.eventBoard}>
+      {events.map(({ name, location, speaker, type, time }) => (
+        <Event
+          key={name}
+          name={name}
+          location={location}
+          speaker={speaker}
+          type={type}
+          start={time.start}
+          end={time.end}
+        />
+      ))}
+    </div>
+  );
 };
